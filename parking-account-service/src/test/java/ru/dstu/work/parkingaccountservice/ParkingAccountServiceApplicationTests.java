ac11331374367e690147be119260655f7365b78a
Roman Esenkov <roma.esenkov@yandex.ru>
2026-02-21 17:21:34 +0300

    fix

---
+++ parking-account-service/src/test/java/ru/dstu/work/parkingaccountservice/ParkingAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dstu.work.parkingaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
